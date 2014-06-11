4c9c69edbba14b0722bb191c0b42c86a9773fe7a
Arne Böckmann <arneboe@tzi.de>
2014-06-11 16:36:34 +0200

    Ringbuffer: more tests

--- tests/testringbuffer.cpp
+++ tests/testringbuffer.cpp
@@ -60,58 +60,118 @@ TEST_CASE( "write/read more than size", "[ringbuffer]" )
     REQUIRE(readData == data[0]);
 }
 
+TEST_CASE("negative size", "[ringbuffer]")
+{
+    Ringbuffer<13> buffer;
+    buffer.open(QIODevice::ReadWrite);
+    char data;
+    REQUIRE(-1 == buffer.write(&data, -10));//-1 should be returned in case of error
+    REQUIRE(buffer.bytesAvailable() == 0);//nothing should have been written
+}
+
+TEST_CASE("zero size", "[ringbuffer]")
+{
+    Ringbuffer<13> buffer;
+    buffer.open(QIODevice::ReadWrite);
+    char data;
+    REQUIRE(0 == buffer.write(&data, 0));
+    REQUIRE(buffer.bytesAvailable() == 0);//nothing should have been written
+}
+
+
+TEST_CASE("complex read write", "[ringbuffer]")
+{
+    Ringbuffer<5> buffer;
+    buffer.open(QIODevice::ReadWrite);
+
+    QByteArray nothing = buffer.read(10);
+    REQUIRE(0 == nothing.size());
+    char data[5] = {42, 43, 44, 45, 46};
+    REQUIRE(5 == buffer.write(&data[0], 5));
+    //now the buffer should be full
+    REQUIRE(!buffer.isWritable());
+    char read[3] = {-1};
+    REQUIRE(3 == buffer.read(&read[0], 3));
+    REQUIRE(read[0] == data[0]);
+    REQUIRE(read[1] == data[1]);
+    REQUIRE(read[2] == data[2]);
+    REQUIRE(buffer.isWritable());
+
+    REQUIRE(3 == buffer.write(&data[0], 5));
+    char read5[5] = {-1};
+    REQUIRE(5 == buffer.read(&read5[0], 5));
+    REQUIRE(!buffer.isReadable());
+    REQUIRE(read5[0] == data[3]);
+    REQUIRE(read5[1] == data[4]);
+    REQUIRE(read5[2] == data[0]);
+    REQUIRE(read5[3] == data[1]);
+    REQUIRE(read5[4] == data[2]);
+
+
+}
+
+
+/**
+ *A simple runnable that writes data to a Ringbuffer
+ */
+template <int T>
+class Writer : public QRunnable
+{
+public:
+    Writer(Ringbuffer<T>& buff) : buff(buff)
+    {
+        setAutoDelete(false);
+    }
+    void run()
+    {
+        for(int i = 0; i < 10000; ++i)
+        {
+            int v = 42;
+            int result = buff.write((char*)&v, sizeof(int));
+        }
+    }
+    Ringbuffer<T>& buff;
+};
+
+/**
+ *A simple runnable that reads data from a Ringbuffer
+ */
+template <int T>
+class Reader : public QRunnable
+{
+public:
+    Reader(Ringbuffer<T>& buff) : buff(buff)
+    {
+        setAutoDelete(false);
+    }
+    void run()
+    {
+        for(int i = 0; i < 10000; ++i)
+        {
+            int result;
+            int size = buff.read((char*)&result, sizeof(int));
+            //whole ints are written => we should always be able to read whole ints or nothing
+            if(size == sizeof(int))
+            {
+                REQUIRE(result == 42);
+            }
+            else
+            {
+                REQUIRE(size == 0);
+            }
+        }
+    }
+    Ringbuffer<T>& buff;
+};
 
 TEST_CASE("threaded read/write", "[ringbuffer]")
 {
-    class Writer : public QRunnable
-    {
-    public:
-        Writer(Ringbuffer<40>& buff) : buff(buff)
-        {
-            setAutoDelete(false);
-        }
-        void run()
-        {
-            for(int i = 0; i < 10000; ++i)
-            {
-                int v = 42;
-                int result = buff.write((char*)&v, sizeof(int));
-            }
-        }
-        Ringbuffer<40>& buff;
-    };
-
-    class Reader : public QRunnable
-    {
-    public:
-        Reader(Ringbuffer<40>& buff) : buff(buff)
-        {
-            setAutoDelete(false);
-        }
-        void run()
-        {
-            for(int i = 0; i < 10000; ++i)
-            {
-                int result;
-                int size = buff.read((char*)&result, sizeof(int));
-                //whole ints are written => we should always be able to read whole ints or nothing
-                if(size == sizeof(int))
-                {
-                    REQUIRE(result == 42);
-                }
-                else
-                {
-                    REQUIRE(size == 0);
-                }
-            }
-        }
-        Ringbuffer<40>& buff;
-    };
-    //Note: this whole test assumes that the size of the buffer is divisible by 4
-    Ringbuffer<40> buff;
+    static const int SIZE = 10 * sizeof(int);
+    //Note: this whole test assumes that the size of the buffer is divisible by sizeof(int)
+    Ringbuffer<SIZE> buff;
     buff.open(QIODevice::ReadWrite);
-    Reader reader(buff);
-    Writer writer(buff);
+    Reader<SIZE> reader(buff);
+    Writer<SIZE> writer(buff);
 
     QThreadPool *threadPool = QThreadPool::globalInstance();
     threadPool->start(&reader);
